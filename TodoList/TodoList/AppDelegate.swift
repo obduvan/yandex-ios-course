2ce2cef39e2cbff330858ef3bdda6b36a8265c6e
obduvan <obduvann@gmail.com>
2021-11-29 03:29:09 +0500

    Удален storyboard

--- TodoList/TodoList/AppDelegate.swift
+++ TodoList/TodoList/AppDelegate.swift
@@ -11,36 +11,21 @@ import CocoaLumberjack
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-
-    var task2: Task2?
+    var window: UIWindow?
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        DDLog.add(DDOSLogger.sharedInstance)
-
-        task2 = Task2()
+        //        DDLog.add(DDOSLogger.sharedInstance)
         
-//        метод для анализа кода на производительность и утечки памяти:
-//        task2?.star_1()
+ 
+        window = UIWindow(frame: UIScreen.main.bounds)
         
-//        метод симулрования утечки памяти:
-//        task2?.star_2_memory()
-
-//        метод симулирования производительности:
-//        task2?.star_2_cpu(n: 1000000000)
+        let itemsController = ItemsViewController()
+     
+//        let navigationController = UINavigationController(rootViewController: itemsController)
+        window?.rootViewController = itemsController
+        window?.makeKeyAndVisible()
         
         return true
     }
-    
-    // MARK: UISceneSession Lifecycle
-    
-    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
-        // Called when a new scene session is being created.
-        // Use this method to select a configuration to create the new scene with.
-        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
-    }
-    
-    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
-        // Called when the user discards a scene session.
-        // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
-        // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
-    }
 }
+
