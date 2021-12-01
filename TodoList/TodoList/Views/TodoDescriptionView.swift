dc722fdd87464572a484dffb0d26d194aa51779a
obduvan <obduvann@gmail.com>
2021-12-02 03:37:06 +0500

    Добавлен просмотр состояния textview. Передача введенного текста  контроллеру через делегат

--- TodoList/TodoList/Views/TodoDescriptionView.swift
+++ TodoList/TodoList/Views/TodoDescriptionView.swift
@@ -8,10 +8,21 @@
 import UIKit
 import Foundation
 
+
+protocol DescriptionViewDelegate {
+    func setDescription(description: String)
+}
+
+
 class TodoDescriptionView: UITextView, UITextViewDelegate {
     
-    let placeholder = "Описание дела"
-    let padding: CGFloat = 10
+    private let placeholder = "Описание дела"
+    private let padding: CGFloat = 10
+    private let maxLinesDescription: Int = 20
+    private var descriptionTodo: String = ""
+
+    var descriptionDelegate: DescriptionViewDelegate?
+    
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
@@ -20,6 +31,7 @@ class TodoDescriptionView: UITextView, UITextViewDelegate {
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        delegate = self
         setupDescription()
     }
     
@@ -29,6 +41,46 @@ class TodoDescriptionView: UITextView, UITextViewDelegate {
         if text.isEmpty{
             text = placeholder
             textColor = UIColor.lightGray
+        }
+    }
+    
+    var numberOfCurrentlyDisplayedLines: Int {
+        let size = self.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
+        
+        return Int(((size.height - self.layoutMargins.top - self.layoutMargins.bottom) /
+                        self.font!.lineHeight))
+    }
+
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == .lightGray {
+            textView.text = nil
+            textView.textColor = .black
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.isEmpty {
+            textView.text = placeholder
+            textView.textColor = .lightGray
+            descriptionTodo = ""
+        }
+        else{
+            descriptionTodo = textView.text
+
+        }
+        
+        descriptionDelegate?.setDescription(description: descriptionTodo)
+        textView.resignFirstResponder()
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        removeTextUntilSatisfying(maxNumberOfLines: maxLinesDescription)
+    }
+    
+    func removeTextUntilSatisfying(maxNumberOfLines: Int) {
+        while numberOfCurrentlyDisplayedLines > maxNumberOfLines {
+            self.text = String(self.text.dropLast())
+            self.layoutIfNeeded()
         }
     }
 }
