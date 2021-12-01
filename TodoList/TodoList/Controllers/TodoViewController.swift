24fdafa49785d727946d04b9e3ae7a609891badc
obduvan <obduvann@gmail.com>
2021-12-02 03:33:10 +0500

    3 Task: поправил отступ от клавиатуры. Методы, отвечающие за расширение textView, вынес в отдельный View class. Добавлены подписки на делегаты DescriptionTextView, ColorTextView Добавлено состояние кнопки "удалить"

--- TodoList/TodoList/Controllers/TodoViewController.swift
+++ TodoList/TodoList/Controllers/TodoViewController.swift
@@ -7,61 +7,70 @@
 
 import UIKit
 
-class TodoViewController: UIViewController, UITextViewDelegate {
+class TodoViewController: UIViewController {
     
-    @IBOutlet weak var datePicket: UIDatePicker!
+    @IBOutlet weak var datePicker: UIDatePicker!
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var todoDescription: UITextView!
-    
-    let placeholderDescription = "Описание дела"
-    let maxLinesDescription: Int = 20
-    
-    var keyboardHeight: CGFloat = 0
-    var lineCursor: CGFloat = 0
-    var lineHeightDiscritpion: CGFloat = 0
-    let standartHeightLine: CGFloat = 16.7
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        setObervers()
-        setKeyboardDismiss()
-     
-        datePicket.backgroundColor = .white
-    }
-    
     
     @IBOutlet weak var datePickerView: UIView!
     @IBOutlet weak var dateDivider: UIView!
     @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var deletingButton: UIButton!
     
+    @IBOutlet weak var colorsStackView: TodoColorsView!
+    @IBOutlet weak var todoDescriptionView: TodoDescriptionView!
+
+    
+    private let keyboardIndent: CGFloat = 30
+    private var chosedColor: UIColor = .white
+    private var DescriptionTodo: String = ""
+    private let deletingButtonStateOn: UIColor = .red
+    private let deletingButtonStateOff: UIColor = .lightGray
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setKeyboardObervers()
+        colorsStackView.delegate = self
+        todoDescriptionView.descriptionDelegate = self
+        
+        dateLabel.text = getFormatData(date: datePicker)
+    }
+
     @IBAction func dateClicked(_ sender: Any) {
         UIView.animate(withDuration: 0.3, animations: {
             
             if self.datePickerView.isHidden {
                 self.datePickerView.isHidden = false
                 self.setDateLabel(dateString: self.dateLabel.text ?? "")
+        
             }
             else {
                 self.datePickerView.isHidden = true
-                self.dateLabelChanged(close: true)
+                self.setDateLabelState(close: true)
             }
         })
         dateDivider.isHidden = !dateDivider.isHidden
     }
     
+    
     @IBAction func datePickerChanged(_ sender: UIDatePicker) {
+
+        let dateString = self.getFormatData(date: sender)
+        setDateLabel(dateString: dateString)
+    }
+    
+    private func getFormatData(date: UIDatePicker) -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.locale = Locale(identifier: "ru_RU")
         dateFormatter.dateFormat = "d MMMM YYYY"
-        let dateString = dateFormatter.string(from: sender.date)
-        
-        setDateLabel(dateString: dateString)
+        return dateFormatter.string(from: date.date)
         
     }
     
-    private func dateLabelChanged(close : Bool) {
+    private func setDateLabelState(close: Bool) {
         if close {
             self.dateLabel.isHidden = true
         }
@@ -73,10 +82,56 @@ class TodoViewController: UIViewController, UITextViewDelegate {
     private func setDateLabel(dateString: String) {
         guard !dateString.isEmpty else { return }
         
-        dateLabelChanged(close: false)
+        setDateLabelState(close: false)
         dateLabel.text = dateString
     }
     
+    @IBAction func buttonCloseClocked(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    private func changeButtonDeletingState() {
+        if DescriptionTodo.isEmpty{
+            self.deletingButton.setTitleColor(deletingButtonStateOff, for: .normal)
+        }
+        else {
+            self.deletingButton.setTitleColor(deletingButtonStateOn, for: .normal)
+        }
+    }
+}
+
+
+
+extension TodoViewController: ColorsViewDelegate {
+    func setChosedColor(color: UIColor) {
+        
+        self.chosedColor = color
+    }
+}
+
+
+extension TodoViewController: DescriptionViewDelegate {
+    func setDescription(description: String) {
+        
+        self.DescriptionTodo = description
+        self.changeButtonDeletingState()
+    }
+}
+
+
+
+extension TodoViewController {
+    
+    public func setKeyboardObervers() {
+        NotificationCenter.default.addObserver(self, selector: #selector(TodoViewController.keyboardWillShow),
+                                               name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(TodoViewController.keyboardWillHide),
+                                               name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        setKeyboardDismiss()
+    }
+    
+    //MARK:- KeyboardObserve
     
     private func setKeyboardDismiss() {
         let tap = UITapGestureRecognizer(target: view, action: #selector(UIView.endEditing))
@@ -84,97 +139,14 @@ class TodoViewController: UIViewController, UITextViewDelegate {
         view.addGestureRecognizer(tap)
     }
     
-    
-    @IBAction func buttonCloseClocked(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
-    }
-}
-
-
-
-
-extension TodoViewController {
-    
-    //MARK:- UITextView Description Delegates
-    
-    public func setObervers() {
-        NotificationCenter.default.addObserver(self, selector: #selector(TodoViewController.keyboardWillShow),
-                                               name: UIResponder.keyboardWillShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(TodoViewController.keyboardWillHide),
-                                               name: UIResponder.keyboardWillHideNotification, object: nil)
-        
-        todoDescription.delegate = self
-    }
-    
-    func textViewDidBeginEditing(_ textView: UITextView) {
-        self.lineHeightDiscritpion = todoDescription.font?.lineHeight ?? standartHeightLine
-        
-        if textView.textColor == .lightGray {
-            textView.text = nil
-            textView.textColor = .black
-        }
-    }
-    
-    var numberOfCurrentlyDisplayedLines: Int {
-        let size = todoDescription.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize)
-        
-        return Int(((size.height - todoDescription.layoutMargins.top - todoDescription.layoutMargins.bottom) /
-                        todoDescription.font!.lineHeight))
-    }
-    
-    
-    func textViewDidEndEditing(_ textView: UITextView) {
-        if textView.text.isEmpty {
-            textView.text = placeholderDescription
-            textView.textColor = .lightGray
-        }
-        
-        textView.resignFirstResponder()
-    }
-    
-    func textViewDidChange(_ textView: UITextView) {
-        removeTextUntilSatisfying(maxNumberOfLines: maxLinesDescription)
-        
-        changeScrollContentOffset()
-    }
-    
-    func changeScrollContentOffset(){
-        
-        guard let cursorPosition = todoDescription.selectedTextRange?.start else { return }
-        
-        var newlineCursor = todoDescription.caretRect(for: cursorPosition).origin.y
-        if newlineCursor.isInfinite {
-            newlineCursor = lineCursor + lineHeightDiscritpion
-        }
-        
-        lineCursor = newlineCursor
-        
-        let distanceToBottom = scrollView.frame.size.height - lineCursor - todoDescription.frame.origin.y + scrollView.contentOffset.y
-        let collapse = distanceToBottom - keyboardHeight
-        
-        if collapse < 20 {
-            UIView.animate(withDuration: 0.3, animations: {
-                self.scrollView.contentOffset = CGPoint(x: self.scrollView.contentOffset.x, y: self.scrollView.contentOffset.y - abs(collapse) + 40)
-            })
-        }
-    }
-    
-    func removeTextUntilSatisfying(maxNumberOfLines: Int) {
-        while numberOfCurrentlyDisplayedLines > maxNumberOfLines {
-            todoDescription.text = String(todoDescription.text.dropLast())
-            todoDescription.layoutIfNeeded()
-        }
-    }
-    
-    //MARK:- KeyboardObserve
-    
     @objc func keyboardWillShow(notification: NSNotification) {
         if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
-            keyboardHeight = keyboardSize.height
+            
+            scrollView.contentInset.bottom = keyboardSize.height + keyboardIndent
         }
     }
     
     @objc func keyboardWillHide(notification: NSNotification) {
-        keyboardHeight = 0
+        scrollView.contentInset.bottom = .zero
     }
 }
